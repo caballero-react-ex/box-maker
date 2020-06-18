e778e7c78f0da4166d0f3490f634f7487ce20fd5
Luis Caballero <mendoza386@gmail.com>
2020-06-18 12:51:47 +0200

    Updates

---
+++ precache-manifest.42eb649ee9dff9891217ed60d6b14cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3ba259c559224009738141f1545c29",
+    "url": "/box-maker/index.html"
+  },
+  {
+    "revision": "a8b7dfc17dd866a33977",
+    "url": "/box-maker/static/css/main.77b7d781.chunk.css"
+  },
+  {
+    "revision": "5425b005ec250faff662",
+    "url": "/box-maker/static/js/2.094db575.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/box-maker/static/js/2.094db575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b7dfc17dd866a33977",
+    "url": "/box-maker/static/js/main.c3e967e5.chunk.js"
+  },
+  {
+    "revision": "91f4ea729812b8acd6ec",
+    "url": "/box-maker/static/js/runtime-main.bcbcb8e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b0a068c6afb2eec3df14ffce22e37a6.js"
+  "/box-maker/precache-manifest.42eb649ee9dff9891217ed60d6b14cc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/box-maker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
