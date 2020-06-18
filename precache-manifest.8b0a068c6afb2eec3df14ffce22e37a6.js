74c7a4467fc15ff26898b7e68b838f4e4ee5f7a9
Luis Caballero <mendoza386@gmail.com>
2020-06-18 12:42:50 +0200

    Updates

---
+++ precache-manifest.8b0a068c6afb2eec3df14ffce22e37a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ec2d6a18a2be345c90406bbfeb4ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16a5f9b06328818bcb0",
+    "url": "/static/css/main.77b7d781.chunk.css"
+  },
+  {
+    "revision": "5425b005ec250faff662",
+    "url": "/static/js/2.094db575.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.094db575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16a5f9b06328818bcb0",
+    "url": "/static/js/main.54302bb6.chunk.js"
+  },
+  {
+    "revision": "285fae51e657c9d39ec7",
+    "url": "/static/js/runtime-main.143bdc9f.js"
+  }
+]);
